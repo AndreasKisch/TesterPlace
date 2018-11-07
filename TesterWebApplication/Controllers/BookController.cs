fa8414f64b827783bf14a8c39d41ddc4df97ad27
Andreas Kisch <Andreas@kisch.se>
2018-11-07 20:13:58 +0100

    work on filter for table data

--- TesterWebApplication/Controllers/BookController.cs
+++ TesterWebApplication/Controllers/BookController.cs
@@ -11,14 +11,16 @@ namespace TesterWebApplication.Controllers
     {
         List<Book> BookList = new List<Book>();
 
+        string sortOrder;
         /// <summary>
         /// Access BooksearchAPI to get list of books
         /// </summary>
         /// <returns></returns>
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string _sortOrder)
         {
             using (HttpResponseMessage res = await Helper.BookAPI.GetAsync("books/Get"))
             {
+                sortOrder = _sortOrder;
                 if (res.IsSuccessStatusCode)
                 {
                     BookList = await res.Content.ReadAsAsync<List<Book>>();
@@ -26,7 +28,34 @@ namespace TesterWebApplication.Controllers
                 }
 
             }
+            ViewBag.BookList = BookList;
+
+            switch (sortOrder)
+            {
+                case "Author":
+                    BookList = BookList.OrderBy(b => b.Author).ToList();
+                    break;
+                case "Title":
+                    BookList = BookList.OrderBy(b => b.Title).ToList();
+                    break;
+                case "Price":
+                    BookList = BookList.OrderBy(b => b.Price).ToList();
+                    break;
+                case "Genre":
+                    BookList = BookList.OrderBy(b => b.Genre).ToList();
+                    break;
+                case "PDate":
+                    BookList = BookList.OrderBy(b => b.PublishDate).ToList();
+                    break;
+                default:
+                    BookList = BookList.OrderBy(b => b.Author).ToList();
+                    break;
+            }
+            
+
+
             return View(BookList);
         }
+        
     }
 }
